95943f461f5897c7113ddf42400adc48392248c0
Max <maxvhanamane@gmail.com>
2022-07-06 11:01:55 +0530

    added some files in .gitignore

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 require("dotenv").config(); // to store JWT_PRIVATE_KEY in .env file
 const mongoose = require("mongoose");
-// const mongoURI = "mongodb://localhost:27017/notebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
 const mongoURI = process.env.DATABASE;
 
 const connectToMongoDB = () => {
